6bd30b29bbd9e02aecd16140da949a5ae2650946
Florian Schick <florian.schick@schick-software.de>
2020-06-18 10:18:06 +0200

    Lambda expression stuff moved to separate class.

---
+++ FS.LinqExplained/ExpressionsExplained.cs
@@ -0,0 +1,182 @@
+﻿using FS.LinqExplained;
+using Humanizer;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace FS.LinqExplained
+{
+    public class ExpressionsExplained
+    {
+        public static void Execute()
+        {
+            var dummyTextLines = new[] {
+                "freilebende gummibärchen gibt es nicht." ,
+                "man kauft sie in packungen an der kinokasse",
+                "dieser kauf ist der beginn einer fast erotischen und sehr ambivalenten beziehung gummibärchen-mensch",
+                "zuerst genießt man.",
+                "dieser genuss umfasst alle sinne.",
+                "man wühlt in den gummibärchen, man fühlt sie.",
+                "gummibärchen haben eine konsistenz wie weichgekochter radiergummi.",
+                "die tastempfindung geht auch ins sexuelle."
+            };
+
+            #region V11 Expression evaluation
+            ExpressionEvaluation.Framework.Where(dummyTextLines, item => item.StartsWith("man")).Dump("results in");
+            ExpressionEvaluation.Framework.Where(dummyTextLines, item => item.StartsWith("man") || item.Contains("beziehung")).Dump("results in");
+            ExpressionEvaluation.Framework.Where(dummyTextLines, item => item.StartsWith("man") && item.Contains("wühlt")).Dump("results in");
+            #endregion
+
+            #region V12 Expression creation
+            var containsMethodInfoV12 = typeof(string).GetMethod(nameof(string.Contains), new[] { typeof(string) });
+
+            // Part 'item' of item => item.Contains("man")
+            var itemParameterExpressionV12 = Expression.Parameter(typeof(string), "item");
+
+            // Part '"man"' of item => item.Contains("man")
+            var valueExpressionV12 = Expression.Constant("man", typeof(string));
+
+            // Part 'item.Contains("man")' of item => item.Contains("man")
+            var stringContainsExpressionV12 = Expression.Call(itemParameterExpressionV12, containsMethodInfoV12, valueExpressionV12);
+
+            // Full 'item => item.Contains("man")'
+            var itemContainsValueLambdaExpressionV12 = Expression.Lambda(stringContainsExpressionV12, itemParameterExpressionV12);
+
+            // Cast to requested expression type
+            var typedItemContainsValueLambdaExpressionV12 = (Expression<Func<string, bool>>)itemContainsValueLambdaExpressionV12;
+
+            // Execute
+            ExpressionEvaluation.Framework.Where(dummyTextLines, typedItemContainsValueLambdaExpressionV12).Dump("results in");
+            #endregion
+
+            #region V13 Parser creation
+            var filterValue = "!man";
+
+            var containsMethodInfoV13 = typeof(string).GetMethod(nameof(string.Contains), new[] { typeof(string) });
+
+            // Part 'item' of item => item.Contains("man")
+            var itemParameterExpressionV13 = Expression.Parameter(typeof(string), "item");
+
+            var negateRequested = filterValue.StartsWith('!');
+            if (negateRequested)
+                filterValue = filterValue.Substring(1);
+
+            // Part '"man"' of item => item.Contains("man")
+            var valueExpressionV13 = Expression.Constant(filterValue, typeof(string));
+
+            // Part 'item.Contains("man")' of item => item.Contains("man")
+            var stringContainsExpressionV13 = Expression.Call(itemParameterExpressionV13, containsMethodInfoV13, valueExpressionV13);
+
+            var stringContainsOrNotExpressionV13 = negateRequested
+                ? (Expression)Expression.Not(stringContainsExpressionV13)
+                : (Expression)stringContainsExpressionV13;
+
+            // Full 'item => item.Contains("man")'
+            var itemContainsOrNotValueLambdaExpressionV13 = Expression.Lambda(stringContainsOrNotExpressionV13, itemParameterExpressionV13);
+
+            // Cast to requested expression type
+            var typedItemContainsOrNotValueLambdaExpressionV13 = (Expression<Func<string, bool>>)itemContainsOrNotValueLambdaExpressionV13;
+
+            // Execute
+            ExpressionEvaluation.Framework.Where(dummyTextLines, typedItemContainsOrNotValueLambdaExpressionV13).Dump("results in");
+            #endregion
+        }
+    }
+}
+
+#region ExpressionEvaluation
+namespace ExpressionEvaluation
+{
+    public static class Framework
+    {
+        public static IEnumerable<TItem> Where<TItem>(this IEnumerable<TItem> list, Expression<Func<TItem, bool>> filterExpression)
+        {
+            var result = new List<TItem>();
+
+            var filterDescription = GetExpressionDescription(filterExpression);
+            filterDescription.Dump("Filters the the list as follow");
+
+            var filterFunction = filterExpression.Compile();
+            foreach (var item in list)
+            {
+                var isMatch = filterFunction(item);
+                if (isMatch)
+                    result.Add(item);
+            }
+
+            return result;
+        }
+
+        private static string GetExpressionDescription(Expression expression)
+        {
+            switch (expression)
+            {
+                case LambdaExpression lambdaExpression:
+                    return GetLambdaExpressionDescription(lambdaExpression);
+                case MethodCallExpression methodCallExpression:
+                    return GetMethodCallDescription(methodCallExpression);
+                case BinaryExpression binaryExpression:
+                    return GetLogicalOperationDescription(binaryExpression);
+                case UnaryExpression unaryExpression:
+                    return GetNegatedExpressionDescription(unaryExpression);
+                default:
+                    throw new NotSupportedException($"Expressions of type {expression.GetType()} are currently unsupported");
+            }
+        }
+
+        private static string GetLambdaExpressionDescription(LambdaExpression lambdaExpression)
+        {
+            var parameterDescription = lambdaExpression.Parameters[0].Name;
+            var bodyDescription = GetExpressionDescription(lambdaExpression.Body);
+            return $"{parameterDescription} {bodyDescription}";
+        }
+
+        private static string GetMethodCallDescription(MethodCallExpression methodCallExpression)
+        {
+            if (methodCallExpression.Arguments.Count > 1)
+                throw new NotSupportedException("Methods calls with none or more than one argument currently unsupported");
+
+            string argument;
+            var argumentExpression = methodCallExpression.Arguments[0];
+            switch (argumentExpression)
+            {
+                case ConstantExpression constantExpression:
+                    argument = constantExpression.Value.ToString();
+                    break;
+                default:
+                    throw new NotSupportedException($"Arguments of type {argumentExpression.GetType()} for method calls currently unsupported");
+            }
+
+            return $"{methodCallExpression.Method.Name.Humanize(LetterCasing.LowerCase)} \"{argument}\"";
+        }
+
+        private static string GetLogicalOperationDescription(BinaryExpression binaryExpression)
+        {
+            switch (binaryExpression.NodeType)
+            {
+                case ExpressionType.OrElse:
+                    return $"{GetExpressionDescription(binaryExpression.Left)} or {GetExpressionDescription(binaryExpression.Right)}";
+                case ExpressionType.AndAlso:
+                    return $"{GetExpressionDescription(binaryExpression.Left)} and {GetExpressionDescription(binaryExpression.Right)}";
+                default:
+                    throw new NotSupportedException($"Node type '{binaryExpression.NodeType}' for binary expressions are currently unsupported");
+            }
+        }
+
+        private static string GetNegatedExpressionDescription(UnaryExpression unaryExpression)
+        {
+            string prefix;
+            switch (unaryExpression.NodeType)
+            {
+                case ExpressionType.Not:
+                    prefix = "not";
+                    break;
+                default:
+                    throw new NotSupportedException($"Node type '{unaryExpression.NodeType}' for unary expressions are currently unsupported");
+            }
+
+            return $"{prefix} {GetExpressionDescription(unaryExpression.Operand)}";
+        }
+    }
+}
+#endregion
--- FS.LinqExplained/LinqExplained.cs
+++ FS.LinqExplained/LinqExplained.cs
@@ -1,9 +1,7 @@
 ﻿using FS.LinqExplained;
-using Humanizer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 
 namespace FS.LinqExplained
 {
@@ -11,7 +9,7 @@ namespace FS.LinqExplained
     {
         public static void Execute()
         {
-            var list = new[] {
+            var dummyTextLines = new[] {
                 "freilebende gummibärchen gibt es nicht." ,
                 "man kauft sie in packungen an der kinokasse",
                 "dieser kauf ist der beginn einer fast erotischen und sehr ambivalenten beziehung gummibärchen-mensch",
@@ -22,148 +20,79 @@ namespace FS.LinqExplained
                 "die tastempfindung geht auch ins sexuelle."
             };
 
-            #region V1
-            // V1: Framework and business logic combined into same function.
+            #region V1: Framework and business logic combined into same function.
             var frameworkV1 = new V1.Framework();
-            frameworkV1.Filter(list, "StartWith", "man").Dump("V1 StartWith");
-            frameworkV1.Filter(list, "Contains", "man").Dump("V1 Contains");
+            frameworkV1.Filter(dummyTextLines, "StartWith", "man").Dump("V1 StartWith");
+            frameworkV1.Filter(dummyTextLines, "Contains", "man").Dump("V1 Contains");
             #endregion
 
-            #region V2
-            // V1: Business logic moved to separate class.
+            #region V2: Business logic moved to separate class.
             var frameworkV2 = new V2.Framework();
-            frameworkV2.Filter(list, "StartWith", "man").Dump("V2 StartWith");
-            frameworkV2.Filter(list, "Contains", "man").Dump("V2 Contains");
+            frameworkV2.Filter(dummyTextLines, "StartWith", "man").Dump("V2 StartWith");
+            frameworkV2.Filter(dummyTextLines, "Contains", "man").Dump("V2 Contains");
             #endregion
 
-            #region V3
-            // V3: Delegate for filter functions created. Loops combined.
+            #region V3: Delegate for filter functions created. Loops combined.
             var frameworkV3 = new V3.Framework();
-            frameworkV3.Filter(list, "StartWith", "man").Dump("V3 StartWith");
-            frameworkV3.Filter(list, "Contains", "man").Dump("V3 Contains");
+            frameworkV3.Filter(dummyTextLines, "StartWith", "man").Dump("V3 StartWith");
+            frameworkV3.Filter(dummyTextLines, "Contains", "man").Dump("V3 Contains");
             #endregion
 
-            #region V4
-            // V4: Parameter 'filterFunction' replaced by delegate.
+            #region V4: Parameter 'filterFunction' replaced by delegate.
             var frameworkV4 = new V4.Framework();
-            frameworkV4.Filter(list, BusinessLogic.StartsWith, "man").Dump("V4 StartWith");
-            frameworkV4.Filter(list, BusinessLogic.Contains, "man").Dump("V4 Contains");
+            frameworkV4.Filter(dummyTextLines, BusinessLogic.StartsWith, "man").Dump("V4 StartWith");
+            frameworkV4.Filter(dummyTextLines, BusinessLogic.Contains, "man").Dump("V4 Contains");
             #endregion
 
-            #region V5
-            // V5: Parameter 'filterFunction' replaced by inline function.
+            #region V5: Parameter 'filterFunction' replaced by inline function.
             var frameworkV5 = new V5.Framework();
-            frameworkV5.Filter(list, (string item, string filter) => item.StartsWith(filter), "man").Dump("V5 StartWith");
-            frameworkV5.Filter(list, (string item, string filter) => item.Contains(filter), "man").Dump("V5 Contains");
+            frameworkV5.Filter(dummyTextLines, (string item, string filter) => item.StartsWith(filter), "man").Dump("V5 StartWith");
+            frameworkV5.Filter(dummyTextLines, (string item, string filter) => item.Contains(filter), "man").Dump("V5 Contains");
             #endregion
 
-            #region V6
-            // V6: Parameter 'filter' (re)moved to inline function.
+            #region V6: Parameter 'filter' (re)moved to inline function.
             var frameworkV6 = new V6.Framework();
-            frameworkV6.Filter(list, (string item) => item.StartsWith("man")).Dump("V6 StartWith");
-            frameworkV6.Filter(list, (string item) => item.Contains("man")).Dump("V6 Contains");
+            frameworkV6.Filter(dummyTextLines, (string item) => item.StartsWith("man")).Dump("V6 StartWith");
+            frameworkV6.Filter(dummyTextLines, (string item) => item.Contains("man")).Dump("V6 Contains");
             #endregion
 
-            #region V7
+            #region V7: Generics added
             // V7A: Type of list made generic. Inline function simplified.
             var frameworkV7A = new V7A.Framework();
-            frameworkV7A.Filter(list, (item) => item.StartsWith("man")).Dump("V7A StartWith");
-            frameworkV7A.Filter(list, (item) => item.Contains("man")).Dump("V7A Contains");
+            frameworkV7A.Filter(dummyTextLines, (item) => item.StartsWith("man")).Dump("V7A StartWith");
+            frameworkV7A.Filter(dummyTextLines, (item) => item.Contains("man")).Dump("V7A Contains");
 
             // V7B: Return type of delegate made generic. Inline function simplified.
             var frameworkV7B = new V7B.Framework();
-            frameworkV7B.Filter(list, item => item.StartsWith("man")).Dump("V7B StartWith");
-            frameworkV7B.Filter(list, item => item.Contains("man")).Dump("V7B Contains");
+            frameworkV7B.Filter(dummyTextLines, item => item.StartsWith("man")).Dump("V7B StartWith");
+            frameworkV7B.Filter(dummyTextLines, item => item.Contains("man")).Dump("V7B Contains");
 
             // V7C: Type parameters marked as co-variant/contra-variant.
             // Covariance: Enables you to use a more derived type than originally specified.
             // Contravariance: Enables you to use a more generic(less derived) type than originally specified.
             var frameworkV7C = new V7C.Framework();
-            frameworkV7C.Filter(list, item => item.StartsWith("man")).Dump("V7C StartWith");
-            frameworkV7C.Filter(list, item => item.Contains("man")).Dump("V7C Contains");
+            frameworkV7C.Filter(dummyTextLines, item => item.StartsWith("man")).Dump("V7C StartWith");
+            frameworkV7C.Filter(dummyTextLines, item => item.Contains("man")).Dump("V7C Contains");
             #endregion
 
-            #region V8
-            // V8: Delegate replace by Func<,>.
+            #region V8: Delegate replace by Func<,>.
             var frameworkV8 = new V8.Framework();
-            frameworkV8.Filter(list, item => item.StartsWith("man")).Dump("V8 StartWith");
-            frameworkV8.Filter(list, item => item.Contains("man")).Dump("V8 Contains");
+            frameworkV8.Filter(dummyTextLines, item => item.StartsWith("man")).Dump("V8 StartWith");
+            frameworkV8.Filter(dummyTextLines, item => item.Contains("man")).Dump("V8 Contains");
             #endregion
 
-            #region V9
-            // V9: Switch to extension method
-            V9.Framework.Filter(list, item => item.StartsWith("man")).Dump("V9 StartWith");
-            V9.Framework.Filter(list, item => item.Contains("man")).Dump("V9 Contains");
+            #region V9: Switch to extension method
+            V9.Framework.Filter(dummyTextLines, item => item.StartsWith("man")).Dump("V9 StartWith");
+            V9.Framework.Filter(dummyTextLines, item => item.Contains("man")).Dump("V9 Contains");
             //list.Filter(item => item.Contains("man")).Dump("V1 StartWith");
             #endregion
 
-            #region V10
-            // V10: 'Filter' renamed to 'Where'
-            V10.Framework.Where(list, item => item.StartsWith("man")).Dump("V10 StartWith");
-            V10.Framework.Where(list, item => item.Contains("man")).Dump("V10 Contains");
+            #region V10: 'Filter' renamed to 'Where'
+            V10.Framework.Where(dummyTextLines, item => item.StartsWith("man")).Dump("V10 StartWith");
+            V10.Framework.Where(dummyTextLines, item => item.Contains("man")).Dump("V10 Contains");
 
-            list.Where(item => item.StartsWith("man")).Dump(".NET Core StartsWith");
-            list.Where(item => item.Contains("man")).Dump(".NET Core Contains");
-            #endregion
-
-            #region V11
-            V11.Framework.Where(list, item => item.StartsWith("man")).Dump("results in");
-            V11.Framework.Where(list, item => item.StartsWith("man") || item.Contains("beziehung")).Dump("results in");
-            V11.Framework.Where(list, item => item.StartsWith("man") && item.Contains("wühlt")).Dump("results in");
-            #endregion
-
-            #region V12
-            var containsMethodInfoV12 = typeof(string).GetMethod(nameof(string.Contains), new[] { typeof(string) });
-
-            // Part 'item' of item => item.Contains("man")
-            var itemParameterExpressionV12 = Expression.Parameter(typeof(string), "item");
-
-            // Part '"man"' of item => item.Contains("man")
-            var valueExpressionV12 = Expression.Constant("man", typeof(string));
-
-            // Part 'item.Contains("man")' of item => item.Contains("man")
-            var stringContainsExpressionV12 = Expression.Call(itemParameterExpressionV12, containsMethodInfoV12, valueExpressionV12);
-
-            // Full 'item => item.Contains("man")'
-            var itemContainsValueLambdaExpressionV12 = Expression.Lambda(stringContainsExpressionV12, itemParameterExpressionV12);
-
-            // Cast to requested expression type
-            var typedItemContainsValueLambdaExpressionV12 = (Expression<Func<string, bool>>)itemContainsValueLambdaExpressionV12;
-
-            // Execute
-            V11.Framework.Where(list, typedItemContainsValueLambdaExpressionV12).Dump("results in");
-            #endregion
-
-            #region V13
-            var filterValue = "!man";
-
-            var containsMethodInfoV13 = typeof(string).GetMethod(nameof(string.Contains), new[] { typeof(string) });
-
-            // Part 'item' of item => item.Contains("man")
-            var itemParameterExpressionV13 = Expression.Parameter(typeof(string), "item");
-
-            var negateRequested = filterValue.StartsWith('!');
-            if (negateRequested)
-                filterValue = filterValue.Substring(1);
-
-            // Part '"man"' of item => item.Contains("man")
-            var valueExpressionV13 = Expression.Constant(filterValue, typeof(string));
-
-            // Part 'item.Contains("man")' of item => item.Contains("man")
-            var stringContainsExpressionV13 = Expression.Call(itemParameterExpressionV13, containsMethodInfoV13, valueExpressionV13);
-
-            var stringContainsOrNotExpressionV13 = negateRequested
-                ? (Expression)Expression.Not(stringContainsExpressionV13)
-                : (Expression)stringContainsExpressionV13;
-
-            // Full 'item => item.Contains("man")'
-            var itemContainsOrNotValueLambdaExpressionV13 = Expression.Lambda(stringContainsOrNotExpressionV13, itemParameterExpressionV13);
-
-            // Cast to requested expression type
-            var typedItemContainsOrNotValueLambdaExpressionV13 = (Expression<Func<string, bool>>)itemContainsOrNotValueLambdaExpressionV13;
-
-            // Execute
-            V11.Framework.Where(list, typedItemContainsOrNotValueLambdaExpressionV13).Dump("results in");
+            dummyTextLines.Where(item => item.StartsWith("man")).Dump(".NET Core StartsWith");
+            dummyTextLines.Where(item => item.Contains("man")).Dump(".NET Core Contains");
             #endregion
         }
     }
@@ -478,103 +407,6 @@ namespace V10
             }
 
             return result;
-        }
-    }
-}
-#endregion
-
-#region V11
-namespace V11
-{
-    public static class Framework
-    {
-        public static IEnumerable<TItem> Where<TItem>(this IEnumerable<TItem> list, Expression<Func<TItem, bool>> filterExpression)
-        {
-            var result = new List<TItem>();
-
-            var filterDescription = GetExpressionDescription(filterExpression);
-            filterDescription.Dump("V11 Filters the the list as follow");
-
-            var filterFunction = filterExpression.Compile();
-            foreach (var item in list)
-            {
-                var isMatch = filterFunction(item);
-                if (isMatch)
-                    result.Add(item);
-            }
-
-            return result;
-        }
-
-        private static string GetExpressionDescription(Expression expression)
-        {
-            switch (expression)
-            {
-                case LambdaExpression lambdaExpression:
-                    return GetLambdaExpressionDescription(lambdaExpression);
-                case MethodCallExpression methodCallExpression:
-                    return GetMethodCallDescription(methodCallExpression);
-                case BinaryExpression binaryExpression:
-                    return GetLogicalOperationDescription(binaryExpression);
-                case UnaryExpression unaryExpression:
-                    return GetNegatedExpressionDescription(unaryExpression);
-                default:
-                    throw new NotSupportedException($"Expressions of type {expression.GetType()} are currently unsupported");
-            }
-        }
-
-        private static string GetLambdaExpressionDescription(LambdaExpression lambdaExpression)
-        {
-            var parameterDescription = lambdaExpression.Parameters[0].Name;
-            var bodyDescription = GetExpressionDescription(lambdaExpression.Body);
-            return $"{parameterDescription} {bodyDescription}";
-        }
-
-        private static string GetMethodCallDescription(MethodCallExpression methodCallExpression)
-        {
-            if (methodCallExpression.Arguments.Count > 1)
-                throw new NotSupportedException("Methods calls with none or more than one argument currently unsupported");
-
-            string argument;
-            var argumentExpression = methodCallExpression.Arguments[0];
-            switch (argumentExpression)
-            {
-                case ConstantExpression constantExpression:
-                    argument = constantExpression.Value.ToString();
-                    break;
-                default:
-                    throw new NotSupportedException($"Arguments of type {argumentExpression.GetType()} for method calls currently unsupported");
-            }
-
-            return $"{methodCallExpression.Method.Name.Humanize(LetterCasing.LowerCase)} \"{argument}\"";
-        }
-
-        private static string GetLogicalOperationDescription(BinaryExpression binaryExpression)
-        {
-            switch (binaryExpression.NodeType)
-            {
-                case ExpressionType.OrElse:
-                    return $"{GetExpressionDescription(binaryExpression.Left)} or {GetExpressionDescription(binaryExpression.Right)}";
-                case ExpressionType.AndAlso:
-                    return $"{GetExpressionDescription(binaryExpression.Left)} and {GetExpressionDescription(binaryExpression.Right)}";
-                default:
-                    throw new NotSupportedException($"Node type '{binaryExpression.NodeType}' for binary expressions are currently unsupported");
-            }
-        }
-
-        private static string GetNegatedExpressionDescription(UnaryExpression unaryExpression)
-        {
-            string prefix;
-            switch (unaryExpression.NodeType)
-            {
-                case ExpressionType.Not:
-                    prefix = "not";
-                    break;
-                default:
-                    throw new NotSupportedException($"Node type '{unaryExpression.NodeType}' for unary expressions are currently unsupported");
-            }
-
-            return $"{prefix} {GetExpressionDescription(unaryExpression.Operand)}";
         }
     }
 }
